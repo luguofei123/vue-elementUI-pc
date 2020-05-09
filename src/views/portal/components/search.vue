eadd00a3d81dad31f5fb07869bff74b997cc7f6e
luguofei <luguofei123@126.com>
2020-05-09 19:19:15 +0800

    搜索框样式修改

--- src/views/portal/components/search.vue
+++ src/views/portal/components/search.vue
@@ -14,7 +14,7 @@ export default {
   .lgf_search{width:1180px;margin: 10px auto;background: #fff;height: 80px;}
   .lgf_search>a{float: left;margin-top: 20px;margin-left: 20px;}
   .lgf_search>div{width: 400px;margin: 0px auto;height: 80px;line-height: 80px;position: relative;}
-  .lgf_search>div>input{height: 40px;border: 2px solid #b3d4fc;width: 400px;text-indent: 5px;}
-  .lgf_search>div>span{display: inline-block;width: 22px;height: 22px;background: url("../../../assets/img/portal/search.png");position: absolute;top: 28px;right: 10px;}
+  .lgf_search>div>input{height: 40px;border: 2px solid #b3d4fc;width: 400px;text-indent: 5px;font-size:12px;line-height:12px;}
+  .lgf_search>div>span{display: inline-block;width: 22px;height: 22px;background: url("../../../assets/img/portal/search.png");position: absolute;top: 30px;right: 10px;}
   .lgf_search>div>span:hover{cursor: pointer;}
 </style>
