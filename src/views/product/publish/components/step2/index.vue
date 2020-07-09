d864d388de36eea1bf89da3b000eee4530b74d7f
luguofei <luguofei123@126.com>
2020-07-09 17:57:24 +0800

    '聚合函数'

--- src/views/product/publish/components/step2/index.vue
+++ src/views/product/publish/components/step2/index.vue
@@ -14,23 +14,23 @@
         </el-col>
       </el-row>
     </el-form>
-    <p>---------------------------------------------------------------------------------------------------------------------------------------</p>
+    <p>-------------------------------------------------------- -------------------------------------------------------------------------------</p>
     <div v-for="(item,index) in submitData" :key="index">
-      <el-form  :model="item" v-for="(item1,index1) in item.prePrdSpecParamValList" :key="index1" label-width="120px">
+      <el-form  :model="item"  :key="index" label-width="120px">
         <el-row :gutter="10">
           <el-col :xs="4" :sm="6" :md="8" :lg="9" :xl="11">
-            <el-form-item :label="item.paramName + ':'">
-              <el-input v-model="item1.paramValName" placeholder=""></el-input>
+            <el-form-item :label="item.paramName + ':'" prop="paramValName"  :rules="{required: true, message: '请输入数中文参数', trigger: 'blur'}">
+              <el-input v-model="item.paramValName" placeholder="" :disabled="item.paramName===''"></el-input>
             </el-form-item>
           </el-col>
           <el-col :xs="4" :sm="6" :md="8" :lg="9" :xl="11">
-            <el-form-item :label="item.paramCode + ':'">
-              <el-input v-model="item1.paramValCode" placeholder=""></el-input>
+            <el-form-item :label="item.paramCode + ':'" prop="paramValCode" :rules="{required: true, message: '请输入英文参数', trigger: 'blur'}">
+              <el-input v-model="item.paramValCode" placeholder="" :disabled="item.paramCode===''"></el-input>
             </el-form-item>
           </el-col>
           <el-col :xs="4" :sm="6" :md="8" :lg="6" :xl="11">
-              <el-button type="danger" icon="el-icon-minus" @click="minus(index,index1)"></el-button>
-              <el-button type="success" icon="el-icon-plus" @click="addList(index,index1)"></el-button>
+              <el-button type="danger" icon="el-icon-minus" @click="minus(index)" v-if="!(index===0)"></el-button>
+              <el-button type="success" icon="el-icon-plus" @click="addList(index)"></el-button>
           </el-col>
         </el-row>
       </el-form>
@@ -54,7 +54,8 @@ export default {
         paramCode: ''
       },
       submitData: [
-        { paramName: '', paramCode: '', prePrdSpecParamValList: [{ paramValName: '', paramValCode: '' }] }
+        // { paramName: '', paramCode: '', prePrdSpecParamValList: [{ paramValName: '', paramValCode: '' }] }
+        { paramName: '', paramCode: '', paramValName: '', paramValCode: '' }
       ]
     }
   },
@@ -87,6 +88,7 @@ export default {
   },
   methods: {
     formChecked () {
+      console.log(this.submitData)
       let isOk
       this.$refs['step2Form'].validate((valid) => {
         if (valid) {
@@ -98,14 +100,47 @@ export default {
       })
       return isOk
     },
-    addList (index, index1) {
-      console.log(index, index1)
-      this.submitData[index].prePrdSpecParamValList.splice(index1, 0, { paramValName: '', paramValCode: '' })
-      // this.submitData.push({ paramName: '', paramCode: '', prePrdSpecParamValList: [{ paramValName: '', paramValCode: '' }] })
+    addList (index) {
+      let arr = [
+        { paramName: '测试1', paramCode: 'test1', paramValName: 'aaa', paramValCode: 'bbb' },
+        { paramName: '测试2', paramCode: 'test2', paramValName: 'aaa2', paramValCode: 'bbb2' },
+        { paramName: '测试1', paramCode: 'test1', paramValName: 'aaa', paramValCode: 'bbb' }
+      ]
+      this.aggregationArray(arr)
+      console.log(index)
+      this.submitData.push({ paramName: this.formTemp.paramName, paramCode: this.formTemp.paramCode, paramValName: '', paramValCode: '' })
     },
-    minus (index, index1) {
-      console.log(index, index1)
-      this.submitData[index].prePrdSpecParamValList.splice(index1, 1)
+    minus (index) {
+      console.log(index)
+      this.submitData.splice(index, 1)
+      this.formTemp.paramName = this.submitData[this.submitData.length - 1].paramName
+      this.formTemp.paramCode = this.submitData[this.submitData.length - 1].paramCode
+    },
+    // 对象数组聚合函数 根据某个字段来合并数组
+    aggregationArray (array) {
+      let arr = []
+      let obj = {}
+      array.forEach((item, index) => {
+        if (!obj[item.paramName]) {
+          // delete item.paramName
+          arr.push({
+            paramName: item.paramName,
+            pramList: [
+              { paramValName: item.paramValName, paramValCode: item.paramValCode }
+            ]
+          })
+          obj[item.paramName] = 1
+        } else {
+          arr.forEach((item1, index1) => {
+            if (item1.paramName === item.paramName) {
+              item1.pramList.push(
+                { paramValName: item.paramValName, paramValCode: item.paramValCode }
+              )
+            }
+          })
+        }
+      })
+      console.log(arr)
     }
   }
 }
