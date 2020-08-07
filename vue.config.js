14124a86d247e682f037ae4a67fb3594f464b0e0
luguofei <luguofei123@126.com>
2020-08-07 17:08:29 +0800

    多页面配置

--- vue.config.js
+++ vue.config.js
@@ -26,27 +26,27 @@ const externals = {
   axios: 'axios'
 }
 const objectPage = {
-  // index: {
-  //   entry: 'src/main.js',
-  //   template: path.resolve(__dirname, 'src/public/index.html'),
-  //   filename: 'index.html',
-  //   title: 'index',
-  //   // 'src/assets/img/favorite.png' 的作用是把该icon放到和index同级目录
-  //   favicon: 'src/assets/img/portal/tel.jpg',
-  //   // inject: 'body',
-  //   chunks: ['chunk-vendors', 'chunk-common', 'index'],
-  //   minify: {
-  //     minimize: true, // 是否打包为最小值 ,没有看到有什么作用，网上没有相关资料
-  //     removeAttributeQuotes: true, // 移除属性的引号（不常用）
-  //     // removeEmptyElements:true,    //删除所有含有空内容的元素。（不常用,慎用）
-  //     removeComments: true, // 带HTML注释
-  //     collapseWhitespace: true, // 去掉空格
-  //     minifyJS: true, // 压缩html里的js 压缩内联js（使用uglify-js进行的压缩）
-  //     minifyCSS: true // 压缩html里的css 压缩内联css（使用clean-css进行的压缩）
-  //   },
-  //   hash: true, // 引入产出的资源时加上哈希避免缓存,在html中的js和css后面加上？hash值
-  //   cdn: cdn
-  // },
+  index: {
+    entry: 'src/main.js',
+    template: path.resolve(__dirname, 'src/public/index.html'),
+    filename: 'index.html',
+    title: 'index',
+    // 'src/assets/img/favorite.png' 的作用是把该icon放到和index同级目录
+    favicon: 'src/assets/img/portal/tel.jpg',
+    // inject: 'body',
+    chunks: ['chunk-vendors', 'chunk-common', 'index'],
+    minify: {
+      minimize: true, // 是否打包为最小值 ,没有看到有什么作用，网上没有相关资料
+      removeAttributeQuotes: true, // 移除属性的引号（不常用）
+      // removeEmptyElements:true,    //删除所有含有空内容的元素。（不常用,慎用）
+      removeComments: true, // 带HTML注释
+      collapseWhitespace: true, // 去掉空格
+      minifyJS: true, // 压缩html里的js 压缩内联js（使用uglify-js进行的压缩）
+      minifyCSS: true // 压缩html里的css 压缩内联css（使用clean-css进行的压缩）
+    },
+    hash: true, // 引入产出的资源时加上哈希避免缓存,在html中的js和css后面加上？hash值
+    cdn: cdn
+  },
   elementui: {
     entry: 'src/views/elementui/elementui.js',
     template: path.resolve(__dirname, 'src/views/elementui/public/elementui.html'),
